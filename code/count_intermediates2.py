7465c11c90cccaa95766f73ec4093d674eca4fff
Joao Meidanis <joao@DESKTOP-TSE4B4J>
2020-04-15 21:46:25 -0300

    code and data, initial commit

---
+++ code/count_intermediates2.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+"""Script to count the number of intermediates to sort one genome into
+another, given the sizes of the breakpoint graph components.
+"""
+
+from math import factorial, ceil, floor
+import sys
+import argparse
+
+def cycle_intermediates(k):
+    """Number of intermediates for a k-cycle."""
+    x = k / 2
+    return int(factorial(k) / (factorial(x) * factorial(x) * (x + 1)))
+
+def path_intermediates(k):
+    """Number of intermediates for a k-path."""
+    v = k + 1
+    return int(factorial(v) / factorial(floor(v / 2)) / factorial(ceil(v / 2)))
+
+def count_intermediates(cycles = [], paths = []):
+    """Computes the number of intermediates of a genome."""
+    total = 1
+
+    for p in paths:
+        total *= path_intermediates(p)
+    for c in cycles:
+        total *= cycle_intermediates(c)
+
+    return total
+
+if __name__ == '__main__':
+    usage = '%(prog)s [-h] [-c [C [C ...]]] [-p [P [P ...]]]'
+    parser = argparse.ArgumentParser(description='Counts number of solutions.',
+                                     usage=usage)
+
+    parser.add_argument('-c', nargs='*', type=int, default=[],
+                        help='list of lengths of cycles')
+    parser.add_argument('-p', nargs='*', type=int, default=[],
+                        help='list of lengths of paths')
+
+    if len(sys.argv) == 1:
+        args = parser.parse_args('-h'.split())
+    else:
+        args = parser.parse_args()
+
+    k_cycles = []
+    for k in args.c:
+        if k < 0:
+            print("ERROR: Negative cycle size.")
+            exit(-1)
+        if k == 0:
+            print("ERROR: Null cycle size.")
+            exit(-1)
+        if k % 2 == 1:
+            print("ERROR: Odd cycle size.")
+            exit(-1)
+        if k > 2:
+            k_cycles.append(k)
+
+    k_paths = []
+    for k in args.p:
+        if k < 0:
+            print("ERROR: Negative path length.")
+            exit(-1)
+        if k > 0:
+            k_paths.append(k)
+
+    print(count_intermediates(paths = k_paths, cycles = k_cycles))
